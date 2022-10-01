bdb7e6e2477d3a0a101bcd61ab1a8539d2d75d10
Óscar Martín <omartinwork@gmail.com>
2022-10-01 20:37:35 +0200

    Extraer lista completa de hoteles

--- src/index.js
+++ src/index.js
@@ -15,19 +15,22 @@ app.listen(3000, () => {
   console.log('Listening on port 3000!');
 });
 
+async function getHotels() {
+  //Se obtienen los datos de los Hoteles Atayala
+  const atalayaHotels = await getAtalayaHotelsData();
+  //Se obtienen los datos de los Hoteles Resort
+  const resortHotels = await getResortHotelsData();
+  //Se concatenan ambos resultados
+  return _.concat(atalayaHotels, resortHotels);
+}
+
 app.get('/hotelList', async (req, res) => {
   try {
-    //Se obtienen los datos de los Hoteles Atayala
-    const atalayaHotels = await getAtalayaHotelsData();
-    //Se obtienen los datos de los Hoteles Resort
-    const resortHotels = await getResortHotelsData();
-    //Se concatenan ambos resultados
-    const allHotels = _.concat(atalayaHotels, resortHotels);
+    const hotels = await getHotels();
+    
     //Se devuelve la respuesta
-    const response = { hotels: allHotels };
+    const response = { hotels };
     res.json(response);
-
-
   } catch (error) {
     console.error(error);
   }
